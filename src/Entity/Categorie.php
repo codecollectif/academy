da57435771e114e5f67a93b84b8bba039af764de
lea <monthieuxlea@gmail.com>
2025-07-18 07:53:13 +0200

    creation de l'entité page, relation manytoone, non nulle avec une suppression automatique d'une page orpheline dépourvue de catégorie

--- src/Entity/Categorie.php
+++ src/Entity/Categorie.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategorieRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategorieRepository::class)]
@@ -15,6 +17,17 @@ class Categorie
 
     #[ORM\Column]
     private array $titleJson = [];
+
+    /**
+     * @var Collection<int, Page>
+     */
+    #[ORM\OneToMany(targetEntity: Page::class, mappedBy: 'Categorie', orphanRemoval: true)]
+    private Collection $pages;
+
+    public function __construct()
+    {
+        $this->pages = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Categorie
     public function setTitleJson(array $titleJson): static
     {
         $this->titleJson = $titleJson;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Page>
+     */
+    public function getPages(): Collection
+    {
+        return $this->pages;
+    }
+
+    public function addPage(Page $page): static
+    {
+        if (!$this->pages->contains($page)) {
+            $this->pages->add($page);
+            $page->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removePage(Page $page): static
+    {
+        if ($this->pages->removeElement($page)) {
+            // set the owning side to null (unless already changed)
+            if ($page->getCategorie() === $this) {
+                $page->setCategorie(null);
+            }
+        }
 
         return $this;
     }
