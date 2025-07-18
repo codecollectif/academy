6037d8f9b8f5a9a47c500308d87c9261f3ce8825
lea <monthieuxlea@gmail.com>
2025-07-18 11:57:50 +0200

    Création de l'entité Page avec relation ManyToOne, non nullable, et orphanremoval activé

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -15,6 +17,17 @@ class Category
 
     #[ORM\Column]
     private array $titleJson = [];
+
+    /**
+     * @var Collection<int, Page>
+     */
+    #[ORM\OneToMany(targetEntity: Page::class, mappedBy: 'Category', orphanRemoval: true)]
+    private Collection $pages;
+
+    public function __construct()
+    {
+        $this->pages = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class Category
     public function setTitleJson(array $titleJson): static
     {
         $this->titleJson = $titleJson;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Page>
+     */
+    public function getPages(): Collection
+    {
+        return $this->pages;
+    }
+
+    public function addPage(Page $page): static
+    {
+        if (!$this->pages->contains($page)) {
+            $this->pages->add($page);
+            $page->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removePage(Page $page): static
+    {
+        if ($this->pages->removeElement($page)) {
+            // set the owning side to null (unless already changed)
+            if ($page->getCategory() === $this) {
+                $page->setCategory(null);
+            }
+        }
 
         return $this;
     }
---
+++ src/Entity/Page.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PageRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PageRepository::class)]
+class Page
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private array $titleJson = [];
+
+    #[ORM\Column]
+    private array $contentJson = [];
+
+    #[ORM\ManyToOne(inversedBy: 'pages')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Category $Category = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitleJson(): array
+    {
+        return $this->titleJson;
+    }
+
+    public function setTitleJson(array $titleJson): static
+    {
+        $this->titleJson = $titleJson;
+
+        return $this;
+    }
+
+    public function getContentJson(): array
+    {
+        return $this->contentJson;
+    }
+
+    public function setContentJson(array $contentJson): static
+    {
+        $this->contentJson = $contentJson;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->Category;
+    }
+
+    public function setCategory(?Category $Category): static
+    {
+        $this->Category = $Category;
+
+        return $this;
+    }
+}
