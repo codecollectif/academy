1c78fea8316f9026e14d11dd84cb8cf65a1a6b9e
Lucas <hureaux.lucas>
2025-10-02 11:37:27 +0200

    add new test

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -3,13 +3,29 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Repository\UserRepository;
 
 class HomeControllerTest extends WebTestCase
 {
-    public function testSomething(): void
+    public function testUserCanGetToIndex(): void
     {
         $client = static::createClient();
         $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testUserCanGoToProfile(): void
+    {
+        $client = static::createClient();
+
+        $userRepository = static::getContainer()->get(UserRepository::class);
+
+        $testUser = $userRepository->findOneByEmail('exemple123@blabla.fr ');
+
+        $client->loginUser($testUser);
+
+        $crawler = $client->request('GET', '/my-profile');
 
         $this->assertResponseIsSuccessful();
     }
