08ca9e3877e084613cd181547a5705350f26bd57
Lucas <hureaux.lucas>
2025-10-02 14:54:38 +0200

    Add mock testing to app_index routing test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,13 +5,20 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\PageRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(PageRepository $pageRepository): Response
     {
-        return $this->render('home/index.html.twig');
+                return $this->render('home/index.html.twig', [
+                    'pages' => $pageRepository->findBy(
+                        array(),
+                        array('id' => 'DESC'),
+                        10
+                    ),
+                ]);
     }
 
     #[Route('/my-profile', name: 'app_my_profile')]
--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -3,13 +3,38 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use App\Repository\UserRepository;
+use App\Repository\PageRepository;
+use App\Entity\Page;
+use App\Entity\Category;
+use App\Controller\HomeController;
 
 class HomeControllerTest extends WebTestCase
 {
     public function testUserCanGetToIndex(): void
     {
         $client = static::createClient();
+
+        $container = static::getContainer();
+
+        $page1 = new Page();
+
+        $page1->setTitleJson(['fr' => 'testPage1']);
+
+        $page2 = new Page();
+
+        $page2->setTitleJson(['fr' => 'testPage2']);
+
+        $pageRepository = $this->createMock(PageRepository::class);
+        $pageRepository->expects(self::once())
+            ->method('findBy')
+            ->willReturn([
+                $page1,
+                $page2
+            ])
+        ;
+
+        $container->set(PageRepository::class, $pageRepository);
+
         $crawler = $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
