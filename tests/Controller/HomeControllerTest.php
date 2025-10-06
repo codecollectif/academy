c06e2089a9ba18866c528612fe298ee45606a1c4
Lucas <hureaux.lucas>
2025-10-06 09:23:48 +0200

    fix test and add blockage to page show pages for non-authenticated users

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace App\Tests\Controller;
 
+use ReflectionClass;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use App\Repository\PageRepository;
 use App\Entity\Page;
@@ -10,19 +11,35 @@ use App\Controller\HomeController;
 
 class HomeControllerTest extends WebTestCase
 {
+    public function setId(object $entity, int $id): void
+    {
+        $reflectionClass = new ReflectionClass($entity);
+        $idProperty = $reflectionClass->getProperty('id');
+        $idProperty->setAccessible(true);
+        $idProperty->setValue($entity, $id);
+    }
+
     public function testUserCanGetToIndex(): void
     {
         $client = static::createClient();
 
         $container = static::getContainer();
 
+        $category = new Category();
+
         $page1 = new Page();
 
+        $reflectionClass = new ReflectionClass($page1);
+
         $page1->setTitleJson(['fr' => 'testPage1']);
+        $page1->setCategory($category);
+        $this->setId($page1, 1);
 
         $page2 = new Page();
 
         $page2->setTitleJson(['fr' => 'testPage2']);
+        $page2->setCategory($category);
+        $this->setId($page2, 1);
 
         $pageRepository = $this->createMock(PageRepository::class);
         $pageRepository->expects(self::once())
