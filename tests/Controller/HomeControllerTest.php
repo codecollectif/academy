34c5654c8dc38205e2ef3308490c6f137ba737f3
Lucas <hureaux.lucas>
2025-10-06 09:23:48 +0200

    fix test and add blockage to page show pages for non-authenticated users

--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -7,6 +7,7 @@ use App\Repository\PageRepository;
 use App\Entity\Page;
 use App\Entity\Category;
 use App\Controller\HomeController;
+use Doctrine\ORM\EntityManagerInterface;
 
 class HomeControllerTest extends WebTestCase
 {
@@ -16,14 +17,25 @@ class HomeControllerTest extends WebTestCase
 
         $container = static::getContainer();
 
+        $manager = $container->get(EntityManagerInterface::class);
+
+        $category = new Category();
+
         $page1 = new Page();
 
         $page1->setTitleJson(['fr' => 'testPage1']);
+        $page1->setCategory($category);
 
         $page2 = new Page();
 
         $page2->setTitleJson(['fr' => 'testPage2']);
+        $page2->setCategory($category);
 
+        $manager->persist($category);
+        $manager->persist($page1);
+        $manager->persist($page2);
+
+        $manager->flush();
         $pageRepository = $this->createMock(PageRepository::class);
         $pageRepository->expects(self::once())
             ->method('findBy')
