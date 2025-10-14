fdf7736cd6d8600e41233340350d9742695a7779
Lucas <hureaux.lucas>
2025-10-14 09:17:12 +0200

    add user blocking features to SectionController

--- src/Controller/SectionController.php
+++ src/Controller/SectionController.php
@@ -17,6 +17,7 @@ final class SectionController extends AbstractController
     #[Route(name: 'app_section_index', methods: ['GET'])]
     public function index(SectionRepository $sectionRepository): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         return $this->render('section/index.html.twig', [
             'sections' => $sectionRepository->findAll(),
         ]);
@@ -25,6 +26,7 @@ final class SectionController extends AbstractController
     #[Route('/new', name: 'app_section_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $section = new Section();
         $form = $this->createForm(SectionType::class, $section);
         $form->handleRequest($request);
@@ -45,6 +47,7 @@ final class SectionController extends AbstractController
     #[Route('/{id}', name: 'app_section_show', methods: ['GET'])]
     public function show(Section $section): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_USER');
         return $this->render('section/show.html.twig', [
             'section' => $section,
         ]);
@@ -53,6 +56,7 @@ final class SectionController extends AbstractController
     #[Route('/{id}/edit', name: 'app_section_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Section $section, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         $form = $this->createForm(SectionType::class, $section);
         $form->handleRequest($request);
 
@@ -71,6 +75,7 @@ final class SectionController extends AbstractController
     #[Route('/{id}', name: 'app_section_delete', methods: ['POST'])]
     public function delete(Request $request, Section $section, EntityManagerInterface $entityManager): Response
     {
+        $this->denyAccessUnlessGranted('ROLE_ADMIN');
         if ($this->isCsrfTokenValid('delete' . $section->getId(), $request->getPayload()->getString('_token'))) {
             $entityManager->remove($section);
             $entityManager->flush();
